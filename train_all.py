1a0c3cf5993d037b056d2820314ebbc79f267bd0
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-10-14 13:23:33 +0800

    bug

--- train_all.py
+++ train_all.py
@@ -44,15 +44,6 @@ def main():
 
 	args = parser.parse_args()
 
-
-    model_urls = {
-        'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-        'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-        'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-        'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-        'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-    }
-
     pytorch_models = {
         'resnet18': models.resnet18(pretrained=True),
         'resnet34': models.resnet34(pretrained=True),
